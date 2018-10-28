fe8457cd7b65d71cc04ab4a052885f23205a3583
Dmitri Samotessov <dimasamo96@gmail.com>
2018-10-28 23:16:14 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,4 +39,14 @@ class TicTacToe
     index.between?(0,8) && !position_taken?(index)
   end
 
+  def turn_count
+    counter=0
+    @board.each do | pos |
+      if pos == "X" || pos == "O"
+        counter += 1
+      end
+    end
+    return counter
+  end
+
 end
