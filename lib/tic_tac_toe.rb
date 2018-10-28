68ff1bb6fe8773c039f66993eea90d613193a023
Dmitri Samotessov <dimasamo96@gmail.com>
2018-10-28 23:14:15 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,8 +27,8 @@ end
     user_input.to_i - 1
   end
 
-  def move(board, index, current_player)
-    board[index] = current_player
+  def move(index, current_player)
+    @board[index] = current_player
   end
 
 end
